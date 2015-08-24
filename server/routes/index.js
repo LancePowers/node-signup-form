2bf97e8c1778f92132a9f8023713e9bbabe0829c
Lance <Lance@thelpstory.com>
2015-08-24 12:18:48 -0600

    exercise complete

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Node Signup' });
 });
-
+router.post('/submit', function(req, res, next){
+  res.render('index', { title: 'success!' });
+});
 module.exports = router;
